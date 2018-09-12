1efc9ebaab1d7ad2d01b8a3454a5a2a689d88719
Caleb Severn <calsev@gmail.com>
2018-09-11 21:37:03 -0800

    Moved nix_baseline attributes from kitchen to attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,7 @@
 # frozen_string_literal: true
+
+tcb = 'secure_apache'
+
+default['nix_baseline']['hostname'] = 'web.calsev.net'
+default['nix_baseline']['firewall']['enable_http'] = true
+default['nix_baseline']['firewall']['enable_https'] = true
