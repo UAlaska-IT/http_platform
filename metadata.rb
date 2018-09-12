83540cfc0ab09155fd2f3cd0a6d3b207ba36c442
Caleb Severn <calsev@gmail.com>
2018-09-11 20:47:10 -0800

    Added metadata

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+name 'secure_apache'
+maintainer 'Caleb Severn'
+maintainer_email 'calnoreply@gmail.com'
+license 'MIT'
+description 'Installs/Configures secure_apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+issues_url 'https://github.com/calsev/secure_apache/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/calsev/secure_apache' if respond_to?(:source_url)
+
+version '0.1.0'
+
+supports 'ubuntu', '>= 16.0'
+supports 'centos', '>= 7.0'
+
+chef_version '>= 14.0' if respond_to?(:chef_version)
+
+depends 'apache'
