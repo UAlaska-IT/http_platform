81619051051981862b8b7f666e46254ddd61c9b5
Caleb Severn <calsev@gmail.com>
2018-09-21 13:19:53 -0800

    Uplifted apache attributes

---
+++ attributes/apache.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+tcb = 'secure_apache'
+
+default[tcb]['www']['document_root'] = '/var/www/html'
+# Do not enable this without a CA cert
+default[tcb]['www']['use_stapling'] = 'Off'
+
+default['apache']['contact'] = node['nix_baseline']['admin_email']
+
+default['apache']['mod_ssl']['cipher_suite'] = node['nix_baseline']['cert']['cipher_string']
