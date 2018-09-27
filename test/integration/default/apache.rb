69fc92bc58cbf83b6a22e33b022a0df7614ad608
Caleb Severn <calsev@gmail.com>
2018-09-27 15:09:55 -0800

    Added tests for apache package and service

---
+++ test/integration/default/apache.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative '../helpers'
+
+node = json('/opt/chef/run_record/last_chef_run_node.json')['automatic']
+
+if node['platform_family'] == 'debian'
+  apache_service = 'apache2'
+elsif node['platform_family'] == 'rhel'
+  apache_service = 'httpd'
+else
+  raise "Platform family not recognized: #{node['platform_family']}"
+end
+
+describe package(apache_service) do
+  it { should be_installed }
+  its(:version) { should match(/^2\.4/) }
+end
+
+describe service(apache_service) do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
