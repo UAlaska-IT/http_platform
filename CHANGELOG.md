692ff001a4547bc3202f9759fb9c65dcbd8dffe2
Caleb Severn <calsev@gmail.com>
2018-10-06 14:50:42 -0800

    Bumped version and added changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog for HTTP Platform Cookbook
 
+## 0.2.0
+
+* Added switch to CA cert for Apache configs
+
 ## 0.1.0
 
 * Initial release
--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/calsev/http_platform/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/calsev/http_platform' if respond_to?(:source_url)
 
-version '0.1.0'
+version '0.2.0'
 
 supports 'ubuntu', '>= 16.0'
 supports 'centos', '>= 7.0'
