438f4e404b54403ab4e02ad8ebb98ac637060b03
Caleb Severn <calsev@gmail.com>
2018-11-29 13:54:45 -0900

    Adapted url paradigm

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,10 @@ maintainer_email 'ua-oit-se@alaska.edu'
 license 'MIT'
 description 'Installs/configures an HTTPS server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-issues_url 'https://github.alaska.edu/oit-cookbooks/http_platform/issues' if respond_to?(:issues_url)
-source_url 'https://github.alaska.edu/oit-cookbooks/http_platform' if respond_to?(:source_url)
+
+git_url = 'https://github.alaska.edu/oit-cookbooks/http_platform'
+source_url git_url if respond_to?(:source_url)
+issues_url "#{git_url}/issues" if respond_to?(:issues_url)
 
 version '0.2.2'
 
