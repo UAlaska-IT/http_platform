dcec461e70af4345f36da78cccf231edaf0f201e
Caleb Severn <calsev@gmail.com>
2018-11-28 16:50:50 -0900

    Fixed some test paths

--- test/integration/helpers.rb
+++ test/integration/helpers.rb
@@ -2,7 +2,7 @@
 
 def cert_public_dir(node)
   if node['platform_family'] == 'debian'
-    dir = '/etc/ssl/certs/'
+    dir = '/etc/ssl/certs'
   elsif node['platform_family'] == 'rhel'
     dir = '/etc/pki/tls/certs'
   else
@@ -13,9 +13,9 @@ end
 
 def cert_private_dir(node)
   if node['platform_family'] == 'debian'
-    dir = '/etc/ssl/private/'
+    dir = '/etc/ssl/private'
   elsif node['platform_family'] == 'rhel'
-    dir = '/etc/pki/tls/private/'
+    dir = '/etc/pki/tls/private'
   else
     raise "Platform family not recognized: #{node['platform_family']}"
   end
@@ -23,15 +23,15 @@ def cert_private_dir(node)
 end
 
 def path_to_self_signed_cert(node)
-  return cert_public_dir(node) + 'funny.business_cert_self_signed.pem'
+  return File.join(cert_public_dir(node),'funny.business_cert_self_signed.pem')
 end
 
 def path_to_private_key(node)
-  return cert_private_dir(node) + 'funny.business_key_self_signed.pem'
+  return File.join(cert_private_dir(node), 'funny.business_key.pem')
 end
 
 def path_to_dh_params(node)
-  return cert_public_dir(node) + 'dh_param.pem'
+  return File.join(cert_public_dir(node), 'dh_param.pem')
 end
 
 def apache_service(node)
