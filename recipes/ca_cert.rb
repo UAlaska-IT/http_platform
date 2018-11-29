dbd5b911da4a75a27af3c0ceb5228390b8ec88ae
Caleb Severn <calsev@gmail.com>
2018-11-28 15:20:57 -0900

    Added creation of CSR

--- recipes/ca_cert.rb
+++ recipes/ca_cert.rb
@@ -1,3 +1,25 @@
 # frozen_string_literal: true
 
 tcb = 'http_platform'
+
+openssl_x509_request path_to_ca_signed_cert do
+  owner 'root'
+  group 'root'
+  mode '0600'
+
+  # Below must match the certificate
+  common_name cert_common_name
+  country node[tcb]['cert']['country']
+  state node[tcb]['cert']['state']
+  city node[tcb]['cert']['locale']
+  org node[tcb]['cert']['organization']
+  org_unit node[tcb]['cert']['org_unit']
+  email cert_email
+
+  key_file path_to_private_key
+  # key_pass
+
+  key_type 'rsa'
+  # key_curve # default 'prime256v1'
+  key_length node[tcb]['cert']['rsa_bits']
+end
