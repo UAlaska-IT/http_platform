03ac782a85de14ee4252f1e3ac65c80f7f0b4abe
Caleb Severn <calsev@gmail.com>
2018-12-05 15:14:58 -0900

    Split cert recipes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,7 @@
 tcb = 'http_platform'
 
 default[tcb]['configure_firewall'] = true
-default[tcb]['configure_cert'] = true
-default[tcb]['configure_ca_cert'] = true
+default[tcb]['configure_cert'] = true # Create a self-signed cert and possibly other certs
+default[tcb]['configure_vault_cert'] = false # Fetch cert from vault also
 default[tcb]['configure_apache'] = true
+default[tcb]['configure_lets_encrypt_cert'] = false # Use certbot to fetch cert also
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,9 +8,11 @@ include_recipe "#{tcb}::firewall" if node[tcb]['configure_firewall']
 
 include_recipe "#{tcb}::cert" if node[tcb]['configure_cert'] # Must be after FQDN is set, so run a baseline first
 
-include_recipe "#{tcb}::ca_cert" if node[tcb]['configure_ca_cert']
+include_recipe "#{tcb}::vault_cert" if node[tcb]['configure_vault_cert']
 
 invalid_config = node[tcb]['configure_apache'] && !node[tcb]['configure_cert']
 raise 'Cannot configure apache without configuring cert' if invalid_config
 
 include_recipe "#{tcb}::apache" if node[tcb]['configure_apache']
+
+include_recipe "#{tcb}::lets_encrypt_cert" if node[tcb]['configure_lets_encrypt_cert']
