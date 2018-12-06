0a1d7984756f02b3b269c1871fa2b2a69c5669cd
Caleb Severn <calsev@gmail.com>
2018-12-06 12:58:12 -0900

    Made evaluation of vault secret lazy

--- recipes/vault_cert.rb
+++ recipes/vault_cert.rb
@@ -5,8 +5,8 @@ tcb = 'http_platform'
 bag = node[tcb]['cert']['vault_data_bag']
 item = node[tcb]['cert']['vault_bag_item'] || node['fqdn']
 key = node[tcb]['cert']['vault_item_key']
-cert = vault_secret(bag, item, key)
 
 file path_to_vault_cert do
-  content cert
+  # must be lazy because hostname may change!
+  content lazy { vault_secret(bag, item, key) } # rubocop:disable Lint/AmbiguousBlockAssociation
 end
