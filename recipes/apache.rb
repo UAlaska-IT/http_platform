acff09d0a3c047ca4a42af44b8f9f3d569fcd359
Caleb Severn <calsev@gmail.com>
2018-12-06 20:19:54 -0900

    Now testing for a logical error in stapling

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -5,6 +5,9 @@ tcb = 'http_platform'
 node.default['apache']['contact'] = node[tcb]['admin_email']
 node.default['apache']['mod_ssl']['cipher_suite'] = http_cipher_suite
 
+staple_error = node[tcb]['apache']['use_stapling'] && use_self_signed_cert?
+raise 'Cannot use stapling with an untrusted certificate' if staple_error
+
 # For apachectl fullstatus
 package 'elinks' do
   only_if { node[tcb]['apache']['install_test_suite'] }
