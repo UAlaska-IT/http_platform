9453b2b39c678aba485e7f8360046578c99d853a
Caleb Severn <calsev@gmail.com>
2018-12-09 09:40:08 -0900

    Made _generate_cert recipe private

--- recipes/local_cert.rb
+++ recipes/local_cert.rb
@@ -5,6 +5,6 @@ tcb = 'http_platform'
 valid_email = node[tcb]['admin_email'] || node[tcb]['cert']['email']
 raise 'Set node[\'http_platform\'][\'admin_email\'] or node[\'http_platform\'][\'cert\'][\'email\']' unless valid_email
 
-include_recipe "#{tcb}::generate_cert"
+include_recipe "#{tcb}::_generate_cert"
 
 include_recipe "#{tcb}::vault_cert" if node[tcb]['configure_vault_cert']
