753921be0af58ea415c7c1ec1e6b214ae4efaf44
Caleb Severn <calsev@gmail.com>
2018-12-09 09:39:18 -0900

    Made _firewall recipe private

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,7 +2,7 @@
 
 tcb = 'http_platform'
 
-include_recipe "#{tcb}::firewall" if node[tcb]['configure_firewall']
+include_recipe "#{tcb}::_firewall" if node[tcb]['configure_firewall']
 
 raise 'Currently only Apache server is supported' unless configure_apache?
 
