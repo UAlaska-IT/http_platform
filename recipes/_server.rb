66d49a9521bf0bc390a594961468f498532115b7
Caleb Severn <calsev@gmail.com>
2018-12-10 13:43:48 -0900

    Flattened recipe call sequence

--- recipes/_server.rb
+++ recipes/_server.rb
@@ -2,8 +2,7 @@
 
 tcb = 'http_platform'
 
-include_recipe "#{tcb}::firewall" if node[tcb]['configure_firewall']
-
+# TODO: Nginx recipe
 raise 'Currently only Apache server is supported' unless configure_apache?
 
 include_recipe "#{tcb}::_apache" if configure_apache?
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,8 +2,12 @@
 
 tcb = 'http_platform'
 
+raise 'http_platform::default configures a server. see ReadMe for use' unless configure_server?
+
 include_recipe "#{tcb}::local_cert" if node[tcb]['configure_cert']
 
-include_recipe "#{tcb}::_server" if configure_server?
+include_recipe "#{tcb}::firewall" if node[tcb]['configure_firewall']
+
+include_recipe "#{tcb}::_server"
 
 include_recipe "#{tcb}::certbot_cert" if node[tcb]['configure_lets_encrypt_cert']
