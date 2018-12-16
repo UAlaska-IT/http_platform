8241ebcd7643d1a0ba8cc21b32349424e1b9ff80
Caleb Severn <calsev@gmail.com>
2018-12-15 17:38:03 -0900

    Chnaged default group for key

--- attributes/cert.rb
+++ attributes/cert.rb
@@ -2,7 +2,7 @@
 
 tcb = 'http_platform'
 
-default[tcb]['cert']['owner_group'] = 'root'
+default[tcb]['cert']['owner_group'] = 'ssl-cert'
 
 default[tcb]['cert']['expiration_days'] = 365
 default[tcb]['cert']['rsa_bits'] = 2048
