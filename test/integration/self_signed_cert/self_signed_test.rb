39c9dc60d86006851f66b47f82598a08e75b5363
Caleb Severn <calsev@gmail.com>
2019-01-02 13:08:55 -0900

    Added test for stapling configurations

--- test/integration/self_signed_cert/self_signed_test.rb
+++ test/integration/self_signed_cert/self_signed_test.rb
@@ -4,6 +4,10 @@ require_relative '../helpers'
 
 node = json('/opt/chef/run_record/last_chef_run_node.json')['automatic']
 
+describe apache_conf(File.join(path_to_conf_available_dir(node), 'ssl-params.conf')) do
+  its('SSLUseStapling') { should eq ['off'] }
+end
+
 # Special fields for this cert
 describe file(path_to_ssl_host_conf(node)) do
   its(:content) { should match "SSLCertificateFile #{path_to_self_signed_cert(node)}" }
