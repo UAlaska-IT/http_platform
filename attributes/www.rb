144d9e9503dfc45ee51744c1d321a17c88a11a20
Caleb Severn <calsev@gmail.com>
2019-01-02 16:59:24 -0900

    Added gates for header policies

--- attributes/www.rb
+++ attributes/www.rb
@@ -16,3 +16,8 @@ default[tcb]['www']['additional_aliases'] = {}
 default[tcb]['www']['redirect_rules'] = []
 
 default[tcb]['www']['rewrite_rules'] = []
+
+default[tcb]['www']['header_policy']['referrer'] = true
+default[tcb]['www']['header_policy']['x_frame'] = true
+default[tcb]['www']['header_policy']['x_content'] = true
+default[tcb]['www']['header_policy']['xss'] = true
