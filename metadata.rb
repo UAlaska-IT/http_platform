a6bfb2087e7b082406e6dc8488c8e438c50b453e
Caleb Severn <calsev@gmail.com>
2019-06-27 14:14:24 -0800

    Purged private github from repo

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ license 'MIT'
 description 'Installs/configures an HTTPS server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-git_url = 'https://github.alaska.edu/oit-cookbooks/http_platform'
+git_url = 'https://github.com/ualaska-it/http_platform'
 source_url git_url if respond_to?(:source_url)
 issues_url "#{git_url}/issues" if respond_to?(:issues_url)
 
