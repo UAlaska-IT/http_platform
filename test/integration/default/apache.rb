c0d902990683ce5cfc3e58f91e89af4cca7d3026
Caleb Severn <calsev@gmail.com>
2019-10-14 16:51:50 -0800

    Move hostname tests to local cert

--- test/integration/default/apache.rb
+++ test/integration/default/apache.rb
@@ -217,25 +217,6 @@ describe file(File.join(sites_available_dir, 'ssl-site.conf')) do
   it { should be_mode 0o640 }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
-
-  ['www.funny.business', 'funny.business', 'www.me.also', 'me.also'].each do |host|
-    # ['www.funny.business', 'funny.business', 'www.localhost', 'localhost', 'www.me.also', 'me.also'].each do |host|
-    its(:content) { should match "ServerName #{host}" }
-  end
-  its(:content) { should_not match(/www\.funny\.business\.error\.log/) }
-  its(:content) { should match 'funny\.business\.error\.log' }
-  # its(:content) { should_not match(/www\.localhost\.error\.log/) }
-  # its(:content) { should match 'localhost\.error\.log' }
-  its(:content) { should_not match(/www\.me\.also\.error\.log/) }
-  its(:content) { should match 'me\.also\.error\.log' }
-
-  its(:content) { should_not match(/www\.funny\.business\.access\.log/) }
-  its(:content) { should match 'funny\.business\.access\.log combined\s+LogLevel warn' }
-  # its(:content) { should_not match(/www\.localhost\.access\.log/) }
-  # its(:content) { should match 'localhost\.access\.log combined\s+LogLevel warn' }
-  its(:content) { should_not match(/www\.me\.also\.access\.log/) }
-  its(:content) { should match 'me\.also\.access\.log combined\s+LogLevel info' }
-
   its(:content) { should match 'Include conf-available/ssl-host.conf' }
 end
 
--- test/integration/local_cert/apache.rb
+++ test/integration/local_cert/apache.rb
@@ -3,3 +3,22 @@
 require_relative '../helpers'
 
 node = json('/opt/chef/run_record/last_chef_run_node.json')['automatic']
+
+describe file(File.join(sites_available_dir, 'ssl-site.conf')) do
+  ['www.funny.business', 'funny.business', 'www.me.also', 'me.also'].each do |host|
+    its(:content) { should match "ServerName #{host}" }
+  end
+  its(:content) { should_not match(/www\.funny\.business\.error\.log/) }
+  its(:content) { should match 'funny\.business\.error\.log' }
+  # its(:content) { should_not match(/www\.localhost\.error\.log/) }
+  # its(:content) { should match 'localhost\.error\.log' }
+  its(:content) { should_not match(/www\.me\.also\.error\.log/) }
+  its(:content) { should match 'me\.also\.error\.log' }
+
+  its(:content) { should_not match(/www\.funny\.business\.access\.log/) }
+  its(:content) { should match 'funny\.business\.access\.log combined\s+LogLevel warn' }
+  # its(:content) { should_not match(/www\.localhost\.access\.log/) }
+  # its(:content) { should match 'localhost\.access\.log combined\s+LogLevel warn' }
+  its(:content) { should_not match(/www\.me\.also\.access\.log/) }
+  its(:content) { should match 'me\.also\.access\.log combined\s+LogLevel info' }
+end
