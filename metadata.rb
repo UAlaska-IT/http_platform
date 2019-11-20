a274e8b9409101d46adc7057f3c8b292d3451515
Caleb Severn <calsev@gmail.com>
2019-11-20 14:17:51 -0900

    Eliminate obsolete metadata

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,10 @@ maintainer 'OIT Systems Engineering'
 maintainer_email 'ua-oit-se@alaska.edu'
 license 'MIT'
 description 'Installs/configures an HTTPS server'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 git_url = 'https://github.com/ualaska-it/http_platform'
-source_url git_url if respond_to?(:source_url)
-issues_url "#{git_url}/issues" if respond_to?(:issues_url)
+source_url git_url
+issues_url "#{git_url}/issues"
 
 version '2.3.0'
 
@@ -23,7 +22,7 @@ supports 'oracle', '>= 6.0'
 # supports 'suse'
 # supports 'opensuse'
 
-chef_version '>= 14.0' if respond_to?(:chef_version)
+chef_version '>= 14.0'
 
 depends 'apache2', '>= 6.0'
 depends 'apt'
